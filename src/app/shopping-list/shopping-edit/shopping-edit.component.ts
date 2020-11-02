e5150bee9f94fa79f5dff36856f39f27192fef1b
pramod <pramod@open-insights.com>
2020-11-02 17:04:46 +0530

    clean up

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -16,14 +16,14 @@ export class ShoppingEditComponent implements OnInit, OnDestroy{
   editedItemIndex: number;
   editedItem: Ingredient;
 
-  constructor(private shoppingListService: ShoppingListService) { }
+  constructor(private slService: ShoppingListService) { }
   ngOnInit() {
-   this.subcription= this.shoppingListService.startedEditing
+   this.subcription= this.slService.startedEditing
    .subscribe(
      (index: number) => {
         this.editedItemIndex = index;
         this.editMode = true;
-        this.editedItem = this.shoppingListService.getIngredient[index];
+        this.editedItem = this.slService.getIngredient[index];
         this.slForm.setValue({
           name : this.editedItem.name,
           amount: this.editedItem.amount
@@ -32,32 +32,30 @@ export class ShoppingEditComponent implements OnInit, OnDestroy{
    );
   }
 
-  onAddItem(form: NgForm){
+  onSubmit(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name,value.amount);
-    if (this.editMode)
-    {
-      this.shoppingListService.updateIngredient(this.editedItemIndex,newIngredient)
+    if (this.editMode) {
+      this.slService.updateIngredient(this.editedItemIndex,newIngredient)
     }else
     {
-      this.shoppingListService.addIngredient(newIngredient);
+      this.slService.addIngredient(newIngredient);
     }
     this.editMode = false;
     form.reset();
   }
 
-  onClear(){
+  onClear() {
     this.slForm.reset();
     this.editMode = false;
   }
 
-  onDelete()
-  {
-    this.shoppingListService.deleteIngredient(this.editedItemIndex);
+  onDelete() {
+    this.slService.deleteIngredient(this.editedItemIndex);
     this.onClear();
   }
 
-  ngOnDestroy(): void {
+  ngOnDestroy() {
     this.subcription.unsubscribe();
   }
 }
