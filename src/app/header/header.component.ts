7be526775794f11a9366b500329f09d79cea6bbc
pramod <pramod@open-insights.com>
2020-11-06 21:13:55 +0530

    lec 284 initial

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,22 @@
 import { Component,  OnInit } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent implements OnInit {
-  constructor() { }
+  constructor(private datastorageservice: DataStorageService) { }
 
   ngOnInit(): void {
+  }
+
+  onSaveData(){
+    this.datastorageservice.storeRecipe();
+  }
+
+  onFetchData()
+  {
+    this.datastorageservice.fetchData().subscribe();
   }
 }
---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Recipe } from '../recipes/recipe.model';
+import { RecipeService } from '../recipes/recipe.service';
+import { map,tap } from 'rxjs/operators';
+
+Injectable({
+    providedIn: 'root'
+})
+export class DataStorageService{
+    constructor(private http: HttpClient,private service: RecipeService){}
+
+    storeRecipe(){
+        const recipes=this.service.getRecipes();
+        this.http.put('https://myproject-ebbe8.firebaseio.com/recipes.json',recipes)
+        .subscribe(response => {
+            console.log(response);
+        });
+    }
+
+    fetchData(){
+        return this.http.get<Recipe[]>('https://myproject-ebbe8.firebaseio.com/recipes.json')
+        .pipe(
+            map(recipes => {
+            return recipes.map(recipe => {
+                return {...recipe,
+                    ingredients : recipe.ingredients ? recipe.ingredients : []
+                };
+            });
+        }),
+        tap(recipes => {
+            this.service.setRecipe(recipes);
+        })
+        )
+    }
+}
