bc37e69e32ae20fb604dcd0fcfe0fa57052c3ba7
pramod <pramod@open-insights.com>
2020-11-09 10:33:40 +0530

    clean up lec 294

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -19,7 +19,7 @@ export class AuthService {
   signup(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(
-        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDb0xTaRAoxyCgvaDF3kk5VYOsTwB_3o7Y',
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA22cZFbywuP79jZTSRDKjS0ETNeybPbOY',
         {
           email: email,
           password: password,
